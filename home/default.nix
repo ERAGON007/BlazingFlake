a9b8e46d968b721494661f35e662e544238a80d1
ERAGON <68738639+ERAGON007@users.noreply.github.com>
2023-01-19 00:35:01 +0330

    Initial commit

---
+++ home/default.nix
@@ -0,0 +1,41 @@
+{ self, inputs, config, ... }:
+{
+  flake = {
+    homeModules = {
+      common = {
+        home.stateVersion = "22.11";
+        imports = [
+          ./tmux.nix
+          ./neovim.nix
+          ./starship.nix
+          ./terminal.nix
+          ./direnv.nix
+        ];
+      };
+      common-linux = {
+        imports = [
+          self.homeModules.common
+          ./vscode-server.nix
+        ];
+        programs.bash.enable = true;
+      };
+      common-darwin = {
+        imports = [
+          self.homeModules.common
+          ./emacs.nix
+        ];
+
+        programs.zsh.enable = true;
+        # This must be envExtra (rather than initExtra), because doom-emacs requires it
+        # https://github.com/doomemacs/doomemacs/issues/687#issuecomment-409889275
+        #
+        # But also see: 'doom env', which is what works.
+        programs.zsh.envExtra = ''
+          export PATH=/etc/profiles/per-user/$USER/bin:/run/current-system/sw/bin/:$PATH
+          # For 1Password CLI. This requires `pkgs.gh` to be installed.
+          source $HOME/.config/op/plugins.sh
+        '';
+      };
+    };
+  };
+}
